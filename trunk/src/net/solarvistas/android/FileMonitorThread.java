c780a4d261049a583b994bdbc34cd542425b73ca
xeno.zhang <xeno.zhang@7c7c8098-20f7-11de-a108-cd2f117ce590>
2009-05-14 03:54:17 +0000

    inotify working

--- trunk/src/net/solarvistas/android/FileMonitorThread.java
+++ trunk/src/net/solarvistas/android/FileMonitorThread.java
@@ -12,7 +12,7 @@ public class FileMonitorThread implements Runnable {
 	String target = "/data/tmp";///sdcard";
 	BackgroundService bs;
 	Process notify, testp;
-	final String app_path = "/data/data/net.ssolarvistas.android/libs/";
+	final String app_path = "/data/";//data/net.ssolarvistas.android/libs/";
 	
     public FileMonitorThread(BackgroundService bs) {
     	this.bs = bs;
@@ -22,16 +22,11 @@ public class FileMonitorThread implements Runnable {
     	try {
     		String status;
     		BufferedReader input = invokeNotify(target);
-    		BufferedReader error = new BufferedReader(new InputStreamReader(notify.getErrorStream()));
-    		BufferedReader test = new BufferedReader(new InputStreamReader(testp.getInputStream()));
-    		Log.d("teledroid", "+=============================================+");
+    		//BufferedReader error = new BufferedReader(new InputStreamReader(notify.getErrorStream()));
+    		//BufferedReader test = new BufferedReader(new InputStreamReader(testp.getInputStream()));
     		while(true){
 				if ((status = input.readLine()) != null)
 					Log.d("teledroid", status);
-				if ((status = error.readLine()) != null)
-					Log.d("teledroid", "[Error]" + status);
-				if ((status = test.readLine()) != null)
-					Log.d("teledroid", "[Test]" + status);
     		}
     	} catch (Exception e) {
 			Log.d("Teledroid.Monitor", "IOException", e);
@@ -41,10 +36,7 @@ public class FileMonitorThread implements Runnable {
     private BufferedReader invokeNotify(String target) throws Exception {
     	String command = app_path + "inotify -c 0 " + target;
 		notify = Runtime.getRuntime().exec(command);
-		Log.d("teledroid", "FileMonitor Initiated Process $" + command);
-		
-		command = app_path + "inotify -c " + target;
-		testp = Runtime.getRuntime().exec(command);
+		Log.d("teledroid", ">> FileMonitor Initiated Process $" + command);
 		return new BufferedReader(new InputStreamReader(notify.getInputStream()));
     }
 }
