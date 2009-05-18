715bdf2e8b7062f6299593e47865f5993f21f302
rictic <rictic@7c7c8098-20f7-11de-a108-cd2f117ce590>
2009-05-18 05:27:38 +0000

    added more debug info

--- trunk/src/net/solarvistas/android/SynThread.java
+++ trunk/src/net/solarvistas/android/SynThread.java
@@ -1,9 +1,7 @@
 package net.solarvistas.android;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 import android.os.Message;
 import android.util.Log;
 
@@ -21,7 +19,7 @@ public class SynThread implements Runnable {
         //Log.d("Server", mMSG);
 		
 		try {
-			getServerInfo(BackgroundService.ssh.Exec("dir-print sdcard\n"));
+			getServerInfo(BackgroundService.ssh.Exec("cat a.txt\n"));
 		} catch (Exception e) {
 			Log.e("teledroid.SynThread.run", "Error getting dir-print info from server");
 			e.printStackTrace();
@@ -30,20 +28,25 @@ public class SynThread implements Runnable {
 	}
 
     private void getServerInfo(Channel channel) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(channel
-								.getInputStream()));
-    	while (true) {
-    		String msg = input.readLine();
-			
-			if (msg.contains("{")){
-				Message m = new Message();
-				m.what = ScanFilesThread.BUMP_MSG;
-				m.obj = msg;
-				scanFiles.getServerInfoHandler.sendMessage(m);
-				channel.disconnect();
-				return;
-			}	  
+        Log.d("teledroid.SynThread.getServerInfo", "reading for JSON object from dir-print on " + channel);
+    	InputStreamReader input = new InputStreamReader(channel.getInputStream());
+    	final int BUFSIZE = 4 * 1028;
+    	char[] buffer = new char[BUFSIZE];
+    	StringBuilder sb = new StringBuilder();
+    	while(true) {
+    		int amount = input.read(buffer, 0, BUFSIZE);
+    		if (amount == -1)
+    			break;
+    		Log.v("teledroid.SynThread.getServerInfo", "read from channel: " + buffer.toString());
+    		sb.append(buffer, 0, amount);
     	}
-       
+    	String msg = sb.toString();
+
+    	Log.d("teledroid.SynThread.getServerInfo","JSON found " + msg);
+		Message m = new Message();
+		m.what = ScanFilesThread.BUMP_MSG;
+		m.obj = msg;
+		scanFiles.getServerInfoHandler.sendMessage(m);
+		channel.disconnect();
     }
 }
