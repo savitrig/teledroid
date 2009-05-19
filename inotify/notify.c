0f5a0660a8d99c7e139a1d128fed12d18398dbd2
xeno.zhang <xeno.zhang@7c7c8098-20f7-11de-a108-cd2f117ce590>
2009-05-19 21:53:20 +0000

    notify jni lib:

--- inotify/notify.c
+++ inotify/notify.c
@@ -17,59 +17,11 @@ int event_pos = 0;
 char event_buf[512];
 struct inotify_event *event;
 
-JNIEXPORT jint JNICALL Java_net_solarvistas_android_Notify_initNotify
-  (JNIEnv *env, jclass clazz){
-	return inotify_init();
-}
-
-JNIEXPORT jint JNICALL Java_net_solarvistas_android_Notify_registerFile
-  (JNIEnv *env, jclass clazz, jint nfd, jstring file, jint mask) {
-	int wd;
-	const jbyte *filename;
-    filename = (*env)->GetStringUTFChars(env, file, NULL);
-	if (filename == NULL) {
-		return -1; /* OutOfMemoryError already thrown */
-	}
-	wd = inotify_add_watch(nfd, file, mask);
-	(*env)->ReleaseStringUTFChars(env, prompt, str);
-	return wd;
-}
-
-JNIEXPORT jlong JNICALL Java_net_solarvistas_android_Notify_nextEvent
-  (JNIEnv *env, jclass clazz){
-	if(res < (int)sizeof(*event)){
-		LOGE("ERROR: event buffer overflow");
-		return -1;
-	}
-
-	int event_size;
-	long event_long;
-	event = (struct inotify_event *)(event_buf + event_pos);		
-	event_long = event->wd * 0x100000000 + event->mask;
-	event_size = sizeof(*event) + event->len;
-	res -= event_size;
-	event_pos += event_size;
-}
-
-JNIEXPORT jboolean JNICALL Java_net_solarvistas_android_Notify_hasNext
-  (JNIEnv *env, jclass clazz, jint nfd){
-	if(res >= (int)sizeof(*event))
-		return true;
-	event_pos = 0;
-	res = read(nfd, event_buf, sizeof(event_buf));
-	if(res < (int)sizeof(*event)) {
-		if(errno != EINTR)
-			LOGE("ERROR: could not get event");
-		return false;
-    }
-	return true;
-}
-
 static JNINativeMethod sMethods[] = {
      /* name, signature, funcPtr */
 	{"initNotify", "()I", (void*)Java_net_solarvistas_android_Notify_initNotify}, 
 	{"registerFile", "(ILjava/lang/String;I)I", (void*)Java_net_solarvistas_android_Notify_registerFile}, 
-	{"hasNext", "()Z", (void*)Java_net_solarvistas_android_Notify_hasNext}, 
+	{"hasNext", "(I)Z", (void*)Java_net_solarvistas_android_Notify_hasNext}, 
 	{"nextEvent","()J", (void*)Java_net_solarvistas_android_Notify_nextEvent}, 
 };
 
@@ -94,12 +46,60 @@ int jniRegisterNativeMethods(JNIEnv* env, const char* className,
     LOGV("Registering %s natives\n", className);
     clazz = (*env)->FindClass(env, className);
     if (clazz == NULL) {
-        LOGE("Native registration unable to find class '%s'\n", className);
+        LOGD("Native registration unable to find class '%s'\n", className);
         return -1;
     }
     if ((*env)->RegisterNatives(env, clazz, gMethods, numMethods) < 0) {
-        LOGE("RegisterNatives failed for '%s'\n", className);
+        LOGD("RegisterNatives failed for '%s'\n", className);
         return -1;
     }
     return 0;
+}
+
+JNIEXPORT jint JNICALL Java_net_solarvistas_android_Notify_initNotify
+  (JNIEnv *env, jclass clazz){
+	return inotify_init();
+}
+
+JNIEXPORT jint JNICALL Java_net_solarvistas_android_Notify_registerFile
+  (JNIEnv *env, jclass clazz, jint nfd, jstring file, jint mask) {
+	int wd;
+	const jbyte *filename;
+    filename = (*env)->GetStringUTFChars(env, file, NULL);
+	if (filename == NULL) {
+		return -1; /* OutOfMemoryError already thrown */
+	}
+	wd = inotify_add_watch(nfd, filename, mask);
+	(*env)->ReleaseStringUTFChars(env, file, filename);
+	return wd;
+}
+
+JNIEXPORT jlong JNICALL Java_net_solarvistas_android_Notify_nextEvent
+  (JNIEnv *env, jclass clazz){
+	if(res < (int)sizeof(*event)){
+		LOGD("ERROR: event buffer overflow");
+		return -1;
+	}
+
+	int event_size;
+	long event_long;
+	event = (struct inotify_event *)(event_buf + event_pos);		
+	event_long = event->wd * 0x100000000 + event->mask;
+	event_size = sizeof(*event) + event->len;
+	res -= event_size;
+	event_pos += event_size;
+}
+
+JNIEXPORT jboolean JNICALL Java_net_solarvistas_android_Notify_hasNext
+  (JNIEnv *env, jclass clazz, jint nfd){
+	if(res >= (int)sizeof(*event))
+		return JNI_TRUE;
+	event_pos = 0;
+	res = read(nfd, event_buf, sizeof(event_buf));
+	if(res < (int)sizeof(*event)) {
+		if(errno != EINTR)
+			LOGD("ERROR: could not get event");
+		return JNI_FALSE;
+    }
+	return JNI_TRUE;
 }
