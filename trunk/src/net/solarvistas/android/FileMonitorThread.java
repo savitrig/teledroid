91561bcdb9a40f95cd44db0646b663d41fee28af
rictic <rictic@7c7c8098-20f7-11de-a108-cd2f117ce590>
2009-05-20 02:13:01 +0000

    Local inotify preparations

--- trunk/src/net/solarvistas/android/FileMonitorThread.java
+++ trunk/src/net/solarvistas/android/FileMonitorThread.java
@@ -1,9 +1,11 @@
 package net.solarvistas.android;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import android.util.Log;
 
@@ -11,13 +13,10 @@ import android.util.Log;
 public class FileMonitorThread implements Runnable {
 
     //Map<String, Long> mFilesMap;
-    BackgroundService bs;
-    
     Process mPNotify;
 	int mRunningStatus;
     String mTarget = "/sdcard/teledroid";
-    List<String> mFileChangeList = new ArrayList<String>();
-
+    Map<String,ModificationInfo> mFileChanges = new LinkedHashMap<String,ModificationInfo>();
 	final String inotify = "/data/inotify ";//data/net.ssolarvistas.android/libs/";
     final ArrayList<String> mask = new ArrayList<String>();
 
@@ -26,8 +25,7 @@ public class FileMonitorThread implements Runnable {
     final int TD_FM_TERMINATING = -1;
     final int TD_FM_RESTARTING = 2;
 
-    public FileMonitorThread(BackgroundService bs) {
-    	this.bs = bs;
+    public FileMonitorThread() {
         InitiateMonitoredMaskList();
     }
 
@@ -73,13 +71,14 @@ public class FileMonitorThread implements Runnable {
     }
     
     private void ProcessINotifyEvent(String input) {
-        String file, event;
-        file = input.substring(3).split(", ")[0];
-        event = input.substring(3).split(", ")[1];
+        String fileName= input.substring(3).split(", ")[0];
+        String event = input.substring(3).split(", ")[1];
         if(mask.contains(event)) {
-            if(!mFileChangeList.contains(file)) {
-                mFileChangeList.add(file);
-                Log.d("teledroid", "added " + file + "into list.");
+            File file = new File(fileName);
+            if (!file.isDirectory()){
+            	ModificationInfo.Kind modificationKind = event.contains("DELETE") ? ModificationInfo.Kind.DELETED : ModificationInfo.Kind.MODIFIED;
+            	mFileChanges.put(fileName, new ModificationInfo(file.lastModified(), modificationKind));
+                Log.d("teledroid", "noticed " + file + " had event:" + event);
             }
         }
         Log.d("teledroid", input);
@@ -89,8 +88,10 @@ public class FileMonitorThread implements Runnable {
         this.mRunningStatus = mRunningStatus;
     }
 
-    public List<String> getMFileChangeList() {
-        return mFileChangeList;
+    public Map<String,ModificationInfo> getLatestChanges() {
+        Map<String,ModificationInfo> result = mFileChanges;
+        mFileChanges = new LinkedHashMap<String,ModificationInfo>();
+    	return result;
     }
 
     public int getMRunningStatus() {
